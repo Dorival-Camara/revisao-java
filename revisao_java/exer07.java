f895603ba0f1517b49e9adfc9344320f94f37f97
Dorival-Camara <garfixsd@gmail.com>
2026-07-28 20:45:05 -0300

    terceiro commit

--- revisao_java/exer07.java
+++ revisao_java/exer07.java
@@ -1,5 +1,21 @@
 import java.util.Scanner;
 
-public class exer07 {
-    
+public class exer07
+ {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] vetor = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Digite o número " + (i + 1) + ": ");
+            vetor[i] = scanner.nextInt();
+        }
+
+        System.out.println("Vetor na ordem inversa:");
+        for (int i = 4; i >= 0; i--) {
+            System.out.println(vetor[i]);
+        }
+
+        scanner.close();
+    }
 }
