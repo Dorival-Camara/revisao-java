e63a50fb2c3eb2c51916f46b88c8cd6190d57653
Dorival_Camara <garfixsd@gmail.com>
2026-08-11 08:37:19 -0300

    Add files via upload

---
+++ revisao-array/jogoDaVelha.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class jogoDaVelha{
+    public static void main (String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        char[][] = new char[3][3];
+        char j1 = scanner.nextInt();
+        char j2 = scanner.nextInt();
+        
+        System.out.println("---------------------------------");
+        System.out.println("        JOGO DA VELHA            ");
+        System.out.println("---------------------------------\n");
+        
+
+    }
+}
